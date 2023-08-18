e477ccb8d055de8839e3b23b9ed62a30b33c5bb0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-18 14:23:50 +0530

    setting up route pages and navbar component

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="h-16 w-full border-b-2 flex items-center justify-between p-2">
+      <ul className="flex">
+        <li className="p-2">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="p-2">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="p-2">
+          <Link href="/profile">Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
