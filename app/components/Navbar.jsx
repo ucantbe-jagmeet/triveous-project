cd6dc5761369daa6383ca5f849c741512c5ed121
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-23 16:05:13 +0530

    displaying user name

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,6 +4,22 @@ import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, googleSignIn, logOut } = UserAuth();
+  console.log(user);
+  const handleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="h-16 w-full border-b-2 flex items-center justify-between p-2">
@@ -18,14 +34,24 @@ const Navbar = () => {
           <Link href="/profile">Profile</Link>
         </li>
       </ul>
-      <ul className="flex">
-        <li className="p-2 cursor-pointer">
-          <Link href="/login">Login</Link>
-        </li>
-        <li className="p-2 cursor-pointer">
-          <Link href="/signup ">Signup</Link>
-        </li>
-      </ul>
+
+      {!user ? (
+        <ul className="flex">
+          <li onClick={handleSignIn} className="p-2 cursor-pointer">
+            Login
+          </li>
+          <li onClick={handleSignOut} className="p-2 cursor-pointer">
+            Signup
+          </li>
+        </ul>
+      ) : (
+        <div className="flex items-center">
+          <p>Welcome, {user.displayName}</p>
+          <p onClick={handleSignOut} className=" ml-7 cursor-pointer">
+            Sign Out
+          </p>
+        </div>
+      )}
     </div>
   );
 };
